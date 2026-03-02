66378c5953b40dbd1558b0e47bd7a74428b46d99
Carlos Santiago <165068458+carlossant77@users.noreply.github.com>
2026-03-02 12:03:40 -0300

    Change allowed origins in CORS configuration

--- src/main/java/br/com/devisecenter/devise_center/infra/configurations/CorsConfiguration.java
+++ src/main/java/br/com/devisecenter/devise_center/infra/configurations/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://devise-center-interface.onrender.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
