19716694b256256a5e8f56170cb7e4ae59606c5f
Carlos Santiago <josesantiag2461@gmail.com>
2026-03-02 21:43:25 -0300

    bugfix: adiciona novamente a configuração de CORS para a permissão da interface oficial

--- src/main/java/br/com/devisecenter/devise_center/infra/configurations/CorsConfiguration.java
+++ src/main/java/br/com/devisecenter/devise_center/infra/configurations/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://devise-center-interface.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
